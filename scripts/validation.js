cadfcaf07951855f8f903088215086cbdbe1953f
Andy <andywickoren@gmail.com>
2023-11-20 19:33:42 -0600

    enablevalidation with togglebutton state.  Need to add escape and overlay

--- scripts/validation.js
+++ scripts/validation.js
@@ -5,39 +5,66 @@ console.log("hello from validation.js");
 // enabling validation by calling enableValidation()
 // pass all the settings on call
 
+//the escape and overlay click is the remaining javascript portion
+
+function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.add(inputErrorClass);
+  errorMessageEl.textContent = inputEl.validationMessage;
+  errorMessageEl.classList.add(errorClass);
+}
+
+function hideInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
+  const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
+  inputEl.classList.remove(inputErrorClass);
+  errorMessageEl.textContent = "";
+  errorMessageEl.classList.remove(errorClass);
+}
+
+function checkInputValidity(formEl, inputEl, options) {
+  if (!inputEl.validity.valid) {
+    return showInputError(formEl, inputEl, options);
+  }
+  hideInputError(formEl, inputEl, options);
+}
+
+function hasInvalidInput(inputList) {
+  return !inputList.every((inputEl) => inputEl.validity.valid);
+}
+
+// disableButton (add the class to disable)
+
+// enableButton (remove it as well)
+
+function toggleButtonState(inputEls, submitButton, { inactiveButtonClass }) {
+  if (hasInvalidInput(inputEls)) {
+    submitButton.classList.add(inactiveButtonClass);
+    submitButton.disabled = true;
+    return;
+  }
+
+  submitButton.classList.remove(inactiveButtonClass);
+  submitButton.disabled = false;
+}
+
+function setEventListeners(formEl, options) {
+  const { inputSelector } = options;
+  const inputEls = [...formEl.querySelectorAll(inputSelector)];
+  const submitButton = formEl.querySelector(".modal__save-button");
+  inputEls.forEach((inputEl) => {
+    inputEl.addEventListener("input", (e) => {
+      checkInputValidity(formEl, inputEl, options);
+      toggleButtonState(inputEls, submitButton, options);
+    });
+  });
+}
+
 function enableValidation(options) {
   const formEls = [...document.querySelectorAll(options.formSelector)];
   formEls.forEach((formEl) => {
     formEl.addEventListener("submit", (e) => {
       e.preventDefault();
     });
-
-    function setEventListeners(formEl, options) {
-      const { inputSelector } = options;
-      const inputEls = [...formEl.querySelectorAll(inputSelector)];
-      inputEls.forEach((inputEl) => {
-        inputEl.addEventListener("input", (e) => {
-          checkInputValidity(formEl, inputEl, options);
-          // toggleButtonState
-        });
-      });
-    }
-
-    function showInputError(formEl, inputEl, { inputErrorClass, errorClass }) {
-      const errorMessageEl = formEl.querySelector(`#${inputEl.id}-error`);
-      inputEl.classList.add(inputErrorClass);
-      errorMessageEl.textContent = inputEl.validationMessage;
-      errorMessageEl.classList.add(options.errorClass);
-    }
-
-    function checkInputValidity(formEl, inputEl, options) {
-      if (!inputEl.validity.valid) {
-        showInputError(formEl, inputEl, options);
-        //   } else {
-        //     hideInputError(formEl, inputEl, options);
-        //   }
-      }
-    }
 
     setEventListeners(formEl, options);
     // checkInputValidity(formEl, inputEl, options);
